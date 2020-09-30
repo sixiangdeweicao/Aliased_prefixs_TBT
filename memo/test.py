b110b64bf147cd3031869cf928a7295c1b262abc
sixiangdeweicao <2292933558@qq.com>
2020-09-30 10:38:14 +0000

    add TBT detcetion

--- memo/test.py
+++
@@ -1,9 +0,0 @@
-
-f=open("/home/sgl/icmp_tbg/TBT/memo/abnormal-prefixes/abnormal-prefixes.txt","r")
-prefixs=set()
-for line in f:
-    if line !="":
-        lines=line.split(" ")
-        prefix=lines[1]
-        prefixs.add(prefix)
-print(len(prefixs))
